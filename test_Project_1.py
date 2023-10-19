23ee18a36bfdc45642b5bcbca1e829b959c7091f
Naqi404 <112116609+Naqi404@users.noreply.github.com>
2023-10-19 14:57:49 -0400

    Updated comment on test case since url builder function has been reworked.

--- test_Project_1.py
+++ test_Project_1.py
@@ -2,11 +2,13 @@ import unittest
 import Project_1
 
 
-def test_get_recent_changes_url(self):
+class TestProject1(unittest.TestCase):
+    #So this test itself should be fine, but you will need to alter it for the new function
+    def test_get_recent_changes_url(self):
         article_name = "Ball_State_University"
         expected_url = (
             "https://en.wikipedia.org/w/api.php?action=query&format=json&"
-            "prop=revisions&titles=Ball State University&rvprop=timestamp|user&"
+            "prop=revisions&titles=Ball_State_University&rvprop=timestamp|user&"
             "rvlimit=30&redirects"
         )
         result_url = Project_1.get_recent_changes_url(article_name)
