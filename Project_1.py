d4d6d014c6ef71a75006d19eddcb36b0b45fd7af
rileyjson <142921318+rileyjson@users.noreply.github.com>
2023-10-19 15:38:39 -0400

    Implemented better SRP and updated test case

--- Project_1.py
+++ Project_1.py
@@ -2,40 +2,43 @@ import json
 import ssl
 from urllib.request import urlopen
 
-def get_recent_changes_data(article_name):
-    changes_url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles={article_name}&rvprop=timestamp|user&rvlimit=30&redirects"
+def get_wikipedia_url(article_name):
+    formatted_name = article_name.replace(' ', '%20')
+    return f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles={formatted_name}&rvprop=timestamp|user&rvlimit=30&redirects"
+
+def fetch_wikipedia_data(url):
     context = ssl._create_unverified_context()
-    response = urlopen(changes_url, context=context)
+    response = urlopen(url, context=context)
     return json.loads(response.read())
 
-def display_recent_changes(data, article_name):
+def display_recent_changes(article_name, data):
     if "query" in data:
         pages = data["query"]["pages"]
-        for _, page_info in pages.items():  #Over here page id is not a used variable, but for clean code practices unpacked using the "_" which is apparantly normal
+        for _, page_info in pages.items():
             if 'redirects' in data['query']:
                 print(f"Redirected to {page_info['title']}")
             
             if "revisions" in page_info:
                 for rev in page_info["revisions"]:
                     print(f"{rev['timestamp']} {rev['user']}")
-            else:
-                print(f"No Wikipedia page found for {article_name}.")
-                return 2
+                return 0
+        print(f"No Wikipedia page found for {article_name}.")
+        return 2
     else:
         print("Network error occurred.")
         return 3
-    return 0
 
 def main():
     article_name = input("Please enter the Wikipedia article name: ").strip()
-    article_name = article_name.replace(' ', '%20')
     
     if not article_name:
         print("You must provide an article name.")
         exit(1)
     
-    data = get_recent_changes_data(article_name)
-    exit_code = display_recent_changes(data, article_name)
+    
+    url = get_wikipedia_url(article_name)
+    data = fetch_wikipedia_data(url)
+    exit_code = display_recent_changes(article_name, data)
     exit(exit_code)
 
 if __name__ == "__main__":
--- test_Project_1.py
+++ test_Project_1.py
@@ -1,16 +1,20 @@
 import unittest
 import Project_1
 
+def get_wikipedia_url(article_name):
+    formatted_name = article_name.replace(' ', '%20')
+    return f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles={formatted_name}&rvprop=timestamp|user&rvlimit=30&redirects"
 
-def test_get_recent_changes_url(self):
-        article_name = "Ball_State_University"
+class TestProject1(unittest.TestCase):
+    def test_get_wikipedia_url(self):
+        article_name = "Ball State University"
         expected_url = (
             "https://en.wikipedia.org/w/api.php?action=query&format=json&"
-            "prop=revisions&titles=Ball State University&rvprop=timestamp|user&"
+            "prop=revisions&titles=Ball%20State%20University&rvprop=timestamp|user&"
             "rvlimit=30&redirects"
         )
-        result_url = Project_1.get_recent_changes_url(article_name)
-        self.assertEqual(result_url, expected_url, "The URL should match the expected one.")
+        url = get_wikipedia_url(article_name)
+        self.assertEqual(url, expected_url, "URL should match the expected one.")
 
 if __name__ == "__main__":
     unittest.main()
